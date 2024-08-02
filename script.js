0a2c0c400bdd94807241860b4d0de8a1db2ac530
ShivrajWandhe0309 <168719288+ShivrajWandhe0309@users.noreply.github.com>
2024-08-02 10:58:13 +0530

    first commit

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ ScrollReveal({
     }); 
 
     ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-    ScrollReveal().reveal('.home-image, .skill-container, .portfolio-box, .contact form', { origin: 'bottom' });
+    ScrollReveal().reveal('.home-image, .skill-container, .portfolio-box, .contact form,.table,.table2', { origin: 'bottom' });
     ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
     ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
     
